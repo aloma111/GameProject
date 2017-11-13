18fe9df02100478b752d39726d2895647a291ebb
Parker Janke <31414473+parkerj123@users.noreply.github.com>
2017-11-12 17:47:29 -0800

    Create MultipleChoiceAnswer.java

---
+++ MultipleChoiceAnswer.java
@@ -0,0 +1,24 @@
+package question;
+
+import java.io.Serializable;
+
+/**
+ * multi-choice answer
+ */
+@SuppressWarnings("serial")
+public class MultiChoiceAnswer extends Answer implements Serializable{
+    private String answer;
+
+    public MultiChoiceAnswer(int questionID, String answerType, char result, String answer) {
+        super(questionID, answerType, result);
+        this.answer = answer;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }    
+}
