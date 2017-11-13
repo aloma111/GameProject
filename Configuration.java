527c6abcca1a6eee270acdf5110b2730742efb78
Parker Janke <31414473+parkerj123@users.noreply.github.com>
2017-11-12 17:44:28 -0800

    Create Configuration.java

---
+++ Configuration.java
@@ -0,0 +1,6 @@
+public class Configuration {
+	
+	public static String STORE_NAME = "GamePrivateKey.store";
+	public static String STORE_PASS = "123456";
+	public static String KEY_PASS = "123456";
+}
