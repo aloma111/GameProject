0a00cb1bec7040c5a784248f00c0f62a1b58739d
Moath Alomar <31414512+aloma111@users.noreply.github.com>
2017-11-28 20:06:42 -0800

    Add code comment

--- QuestionGenerator.java
+++ QuestionGenerator.java
@@ -1,16 +1,34 @@
+
 /**
- * Singleton pattern to get the questions
+ * 
+ * Author: Parker Janke
+ * Student ID: 2251871
+ * Course number: CPSC 353
+ * Section, assignment or exercise number: Class Project
+ * Description: Singleton pattern to get the questions
+ * This class loads questions from file into list
+ * Client code can get the random question from this class
+ * 
  */
 public class QuestionGenerator {
 	
+	/**
+	 * a unique instance object of QuestionGenerator
+	 */
 	private static QuestionGenerator questionGenerator;
 	
-	//private constructor
+	/**
+	 * private constructor
+	 * the client code cannot create this object
+	 */
 	private QuestionGenerator(){
 		
 	}
 	
-	//get instance
+	/**
+	 * get instance of QuestionGenerator
+	 * @return a unique instance object of QuestionGenerator
+	 */
 	public static QuestionGenerator instance(){
 		if (questionGenerator == null){
 			questionGenerator = new QuestionGenerator();
