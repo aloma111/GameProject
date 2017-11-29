990bd6332ffce8c4abebf75b1f965a6e3f97841f
Parker Janke <31414473+parkerj123@users.noreply.github.com>
2017-11-29 13:08:38 -0800

    adds BufferedReader

--- GameClient.java
+++ GameClient.java
@@ -53,10 +53,11 @@ public class GameClient {
 
 			//read a line and simply print on standard output
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			String line = in.readLine();
+			BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+			String line = inFromUser.readLine();
 			while (line != null) {
-				System.out.println(line);
-				line = in.readLine();
+				out.write(line.getBytes());
+				line = inFromUser.readLine();
 			}
 
 			//close resource
