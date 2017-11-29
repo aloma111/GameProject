e7551e50541111633a0774b95745082921158c21
Moath Alomar <31414512+aloma111@users.noreply.github.com>
2017-11-28 20:08:28 -0800

    add code comment for Client

--- GameClient.java
+++ GameClient.java
@@ -1,3 +1,16 @@
+
+/**
+ * 
+ * Author: Moath Alomar
+ * Student ID: 2283736
+ * Course number: CPSC 353
+ * Section, assignment or exercise number: Class Project
+ * Description: Game client open a secure connection to the TCP secure socket server
+ * read the command and send to server
+ * Any response from server will be printed on standard output.
+ * This response is handled by a separated thread
+ */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,48 +20,64 @@ import java.net.UnknownHostException;
 
 import javax.net.ssl.SSLSocketFactory;
 
-/**
- * Game client
- */
 public class GameClient {
 
-	//server host
-	private static String SERVER_HOST = "localhost";
-	
-	//server port
-	private static int SERVER_PORT = 1234;		
-		
-	public void run(){
-		
-		 System.setProperty("javax.net.ssl.trustStore", Configuration.STORE_NAME);
-		 
-		 SSLSocketFactory sf = (SSLSocketFactory) SSLSocketFactory.getDefault();
-		 try {
+	/**
+	 * server host
+	 */
+	private static String SERVER_HOST = "127.0.0.1";
+
+	/**
+	 * server port
+	 */
+	private static int SERVER_PORT = 1234;
+
+	/**
+	 * run method, this method is called by start method The method read command
+	 * from user and send to the server
+	 */
+	public void run() {
+
+		//create SSLSocketFactory object with store
+		System.setProperty("javax.net.ssl.trustStore", Configuration.STORE_NAME);
+		SSLSocketFactory sf = (SSLSocketFactory) SSLSocketFactory.getDefault();
+
+		try {
+			//create socket
 			Socket socket = sf.createSocket(SERVER_HOST, SERVER_PORT);
-			
-			//write some words
+
+			// write some words
 			OutputStream out = socket.getOutputStream();
 			out.write("hello\n".getBytes());
 			out.flush();
-			
+
+			//read a line and simply print on standard output
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			String line = in.readLine();
-			while (line != null){
+			while (line != null) {
 				System.out.println(line);
 				line = in.readLine();
 			}
-			
-		    out.close();
-		    socket.close();
-			
+
+			//close resource
+			out.close();
+			socket.close();
+
 		} catch (UnknownHostException e) {
+			//host not found, simply print on standard output
 			e.printStackTrace();
 		} catch (IOException e) {
+			//I/O exception, simply print on standard output
 			e.printStackTrace();
 		}
 	}
-	//main method
+
+	/**
+	 * main method to start java application
+	 * @param args the program argument
+	 */
 	public static void main(String[] args) {
+		//create GameClient object and calls its run method
 		GameClient client = new GameClient();
 		client.run();
 	}
