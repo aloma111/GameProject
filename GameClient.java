a7e104605423a57e632affe91d4b21668530101d
Parker Janke <31414473+parkerj123@users.noreply.github.com>
2017-11-29 12:51:12 -0800

    Update GameClient.java

--- GameClient.java
+++ GameClient.java
@@ -49,7 +49,7 @@ public class GameClient {
 			// write some words
 			OutputStream out = socket.getOutputStream();
 			out.write("hello\n".getBytes());
-			//out.flush();
+			out.flush();
 
 			//read a line and simply print on standard output
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
