48cedc131243faabab0718d3db65824f1882d67f
Moath Alomar <31414512+aloma111@users.noreply.github.com>
2017-12-03 07:19:27 -0800

    add question, answer to message

--- Message.java
+++ Message.java
@@ -1,6 +1,52 @@
 import java.io.Serializable;
 
+import question.Answer;
+import question.Question;
+
 @SuppressWarnings("serial")
 public class Message implements Serializable{
+	
+	/**
+	 * question
+	 */
+	private Question question;
+	
+	/**
+	 * answer
+	 */
+	private Answer answer;
 
+	/**
+	 * accessor method of question
+	 * return question question
+	 */
+	public Question getQuestion() {
+		return question;
+	}
+
+	/**
+	 * mutator method of question
+	 * @param question question
+	 */
+	public void setQuestion(Question question) {
+		this.question = question;
+	}
+
+	/**
+	 * accessor method of answer
+	 * @return answer answer
+	 */
+	public Answer getAnswer() {
+		return answer;
+	}
+
+	/**
+	 * mutator method of answer
+	 * @param question answer
+	 */
+	public void setAnswer(Answer answer) {
+		this.answer = answer;
+	}
+	
+	
 }
